2e8bd05dccff914cccb6ca857e22e3f5f22fcf8d
Rac <rac75116@gmail.com>
2025-05-02 20:46:39 +0900

    時間のチェックを追加

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const { pathname, search } = request.nextUrl;
+
+    if (new Date() >= new Date("2025-05-02T20:44:00+09:00")) {
+        return NextResponse.rewrite(new URL(pathname + search, "http://localhost:3000"));
+    }
+    return NextResponse.next();
+}
