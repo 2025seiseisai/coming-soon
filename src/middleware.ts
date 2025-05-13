a1f9f19a8a1bb2cb5e970fa40aa0d3d695ec2e5c
Rac <rac75116@gmail.com>
2025-05-13 18:46:37 +0900

    時刻を変更

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
     const { pathname, search } = request.nextUrl;
 
-    if (new Date() >= new Date("2025-05-14T15:00:00+09:00")) {
+    if (new Date() >= new Date("2025-05-14T15:30:00+09:00")) {
         return NextResponse.rewrite(new URL(pathname + search, "http://localhost:3000"));
     }
     return NextResponse.next();
